23120d0be20ae4585189676594d3a285cb44f3d1
VTV02 <93756924+VTV02@users.noreply.github.com>
2024-05-31 08:34:48 +0700

    Create tim.h

---
+++ Include/tim.h
@@ -0,0 +1,14 @@
+/*
+ * tim.h
+ *
+ *  Created on: May 31, 2024
+ *      Author: VOVAN
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+
+void tim2_1hz_init(void);
+#define SR_UIF     (1U<<0)
+
+#endif /* TIM_H_ */
